bfbfa72143212e31ab03d803cdbfd8f6c0db3b31
ayarem <ayarem21@gmail.com>
2023-10-29 14:31:11 +0200

    add ability of table scroll

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -32,12 +32,12 @@ class _HomePageState extends State<HomePage> {
 
   final TextEditingController _salaryController = TextEditingController();
   final TextEditingController _gettingSalaryDateController =
-      TextEditingController();
+  TextEditingController();
 
   void showMyForm(int? id) async {
     if (id != null) {
       final existingData =
-          salaryHistories.firstWhere((element) => element['id'] == id);
+      salaryHistories.firstWhere((element) => element['id'] == id);
       _salaryController.text = existingData['salary'].toString();
       _gettingSalaryDateController.text =
           existingData['getting_salary_date'].toString();
@@ -51,92 +51,98 @@ class _HomePageState extends State<HomePage> {
         elevation: 5,
         isDismissible: false,
         isScrollControlled: true,
-        builder: (_) => Container(
-            padding: EdgeInsets.only(
-              top: 15,
-              left: 15,
-              right: 15,
-              bottom: MediaQuery.of(context).viewInsets.bottom + 120,
-            ),
-            child: Form(
-              key: formKey,
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  TextFormField(
-                    controller: _salaryController,
-                    keyboardType: TextInputType.number,
-                    validator: formValidator,
-                    decoration: const InputDecoration(hintText: 'Enter salary'),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  TextFormField(
-                      validator: formValidator,
-                      controller: _gettingSalaryDateController,
-                      decoration: const InputDecoration(
-                          hintText: 'Getting salary date'),
-                      onTap: () async {
-                        DateTime? pickedDate = await showDatePicker(
-                            context: context,
-                            initialDate: DateTime.now(),
-                            firstDate: DateTime(2000),
-                            //DateTime.now() - not to allow to choose before today.
-                            lastDate: DateTime(2101));
+        builder: (_) =>
+            Container(
+                padding: EdgeInsets.only(
+                  top: 15,
+                  left: 15,
+                  right: 15,
+                  bottom: MediaQuery
+                      .of(context)
+                      .viewInsets
+                      .bottom + 120,
+                ),
+                child: Form(
+                  key: formKey,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      TextFormField(
+                        controller: _salaryController,
+                        keyboardType: TextInputType.number,
+                        validator: formValidator,
+                        decoration: const InputDecoration(
+                            hintText: 'Enter salary'),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      TextFormField(
+                          validator: formValidator,
+                          controller: _gettingSalaryDateController,
+                          decoration: const InputDecoration(
+                              hintText: 'Getting salary date'),
+                          onTap: () async {
+                            DateTime? pickedDate = await showDatePicker(
+                                context: context,
+                                initialDate: DateTime.now(),
+                                firstDate: DateTime(2000),
+                                //DateTime.now() - not to allow to choose before today.
+                                lastDate: DateTime(2101));
 
-                        if (pickedDate != null) {
-                          String formattedDate =
+                            if (pickedDate != null) {
+                              String formattedDate =
                               DateFormat('yyyy-MM-dd').format(pickedDate);
 
-                          setState(() {
-                            _gettingSalaryDateController.text = formattedDate;
-                          });
-                        }
-                      }),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.red,
-                            foregroundColor: Colors.white,
-                          ),
-                          onPressed: () {
-                            Navigator.pop(context);
-                          },
-                          child: const Text("Exit")),
-                      ElevatedButton(
-                        onPressed: () async {
-                          if (formKey.currentState!.validate()) {
-                            if (id == null) {
-                              await addItem();
+                              setState(() {
+                                _gettingSalaryDateController.text =
+                                    formattedDate;
+                              });
                             }
-
-                            if (id != null) {
-                              await updateItem(id);
-                            }
-
-                            // Clear the text fields
-                            setState(() {
-                              _salaryController.text = '';
-                              _gettingSalaryDateController.text = '';
-                            });
-
-                            Navigator.pop(context);
-                          }
-                        },
-                        child: Text(id == null ? 'Create New' : 'Update'),
+                          }),
+                      const SizedBox(
+                        height: 20,
                       ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.red,
+                                foregroundColor: Colors.white,
+                              ),
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                              child: const Text("Exit")),
+                          ElevatedButton(
+                            onPressed: () async {
+                              if (formKey.currentState!.validate()) {
+                                if (id == null) {
+                                  await addItem();
+                                }
+
+                                if (id != null) {
+                                  await updateItem(id);
+                                }
+
+                                // Clear the text fields
+                                setState(() {
+                                  _salaryController.text = '';
+                                  _gettingSalaryDateController.text = '';
+                                });
+
+                                Navigator.pop(context);
+                              }
+                            },
+                            child: Text(id == null ? 'Create New' : 'Update'),
+                          ),
+                        ],
+                      )
                     ],
-                  )
-                ],
-              ),
-            )));
+                  ),
+                )));
   }
 
   String? formValidator(String? value) {
@@ -173,66 +179,60 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Money app'),
-      ),
-      body: _isLoading
-          ? const Center(
-              child: CircularProgressIndicator(),
-            )
-          : salaryHistories.isEmpty
-              ? const Center(child: Text("No Data Available!!!"))
-              : DataTable(
+        appBar: AppBar(
+          title: const Text('Money app'),
+        ),
+        body: _isLoading
+            ? const Center(
+          child: CircularProgressIndicator(),
+        )
+            : salaryHistories.isEmpty
+            ? const Center(child: Text("No Data Available!!!"))
+            : SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: DataTable(
+                  columnSpacing: 75,
                   columns: const <DataColumn>[
-                      DataColumn(
-                        label: Expanded(
-                          child: Text(
-                            'Salary',
-                            style: TextStyle(fontStyle: FontStyle.italic),
-                          ),
-                        ),
+                    DataColumn(
+                      label: Text(
+                        'Salary',
+                        style: TextStyle(fontStyle: FontStyle.italic),
                       ),
-                      DataColumn(
-                        label: Expanded(
-                          child: Text(
-                            'Getting Date',
-                            style: TextStyle(fontStyle: FontStyle.italic),
-                          ),
-                        ),
+                    ),
+                    DataColumn(
+                      label: Text(
+                        'Getting Date',
+                        style: TextStyle(fontStyle: FontStyle.italic),
+
                       ),
-                      DataColumn(
-                        label: Expanded(
-                          child: Text(
-                            'Static tax',
-                            style: TextStyle(fontStyle: FontStyle.italic),
-                          ),
-                        ),
+                    ),
+                    DataColumn(
+                      label: Text(
+                        'Static tax',
+                        style: TextStyle(fontStyle: FontStyle.italic),
                       ),
-                      DataColumn(
-                        label: Expanded(
-                          child: Text(
-                            '5% tax',
-                            style: TextStyle(fontStyle: FontStyle.italic),
-                          ),
-                        ),
+                    ),
+                    DataColumn(
+                      label: Text(
+                        '5% tax',
+                        style: TextStyle(fontStyle: FontStyle.italic),
                       ),
-                      DataColumn(
-                        label: Expanded(
-                          child: Text(
-                            'Total tax',
-                            style: TextStyle(fontStyle: FontStyle.italic),
-                          ),
-                        ),
+                    ),
+                    DataColumn(
+                      label: Text(
+                        'Total tax',
+                        style: TextStyle(fontStyle: FontStyle.italic),
                       ),
-                      DataColumn(
-                        label: Expanded(
-                          child: Text(
-                            'Clear salary',
-                            style: TextStyle(fontStyle: FontStyle.italic),
-                          ),
-                        ),
+                    ),
+                    DataColumn(
+                      label: Text(
+                        'Clear salary',
+                        style: TextStyle(fontStyle: FontStyle.italic),
                       ),
-                    ],
+                    ),
+                  ],
                   rows: List.generate(salaryHistories.length, (index) {
                     double fullTax = salaryHistories[index]["static_tax"] +
                         salaryHistories[index]["five_percent_tax"];
@@ -242,7 +242,7 @@ class _HomePageState extends State<HomePage> {
                       DataCell(
                           Text(salaryHistories[index]["salary"].toString())),
                       DataCell(Text(salaryHistories[index]
-                              ["getting_salary_date"]
+                      ["getting_salary_date"]
                           .toString())),
                       DataCell(Text(
                           salaryHistories[index]["static_tax"].toString())),
@@ -252,33 +252,36 @@ class _HomePageState extends State<HomePage> {
                       DataCell(Text(clearSalary.toString())),
                     ]);
                   })),
+            )
+        ),
 
-      // child: ListTile(
-      //     title: Text(myData[index]['salary'].toString()),
-      //     subtitle: Text(myData[index]['getting_salary_date'].toString()),
-      //     trailing: SizedBox(
-      //       width: 100,
-      //       child: Row(
-      //         children: [
-      //           IconButton(
-      //             icon: const Icon(Icons.edit),
-      //             onPressed: () =>
-      //                 showMyForm(myData[index]['id']),
-      //           ),
-      //           IconButton(
-      //             icon: const Icon(Icons.delete),
-      //             onPressed: () =>
-      //                 deleteItem(myData[index]['id']),
-      //           ),
-      //         ],
-      //       ),
-      //     )),
-      //   ),
-      // ),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
-        onPressed: () => showMyForm(null),
-      ),
+        // child: ListTile(
+        //     title: Text(myData[index]['salary'].toString()),
+        //     subtitle: Text(myData[index]['getting_salary_date'].toString()),
+        //     trailing: SizedBox(
+        //       width: 100,
+        //       child: Row(
+        //         children: [
+        //           IconButton(
+        //             icon: const Icon(Icons.edit),
+        //             onPressed: () =>
+        //                 showMyForm(myData[index]['id']),
+        //           ),
+        //           IconButton(
+        //             icon: const Icon(Icons.delete),
+        //             onPressed: () =>
+        //                 deleteItem(myData[index]['id']),
+        //           ),
+        //         ],
+        //       ),
+        //     )),
+        //   ),
+        // ),
+        floatingActionButton: FloatingActionButton(
+    child: const Icon(Icons.add),
+    onPressed: () => showMyForm(null),
+    )
+    ,
     );
   }
 }
